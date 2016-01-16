2e3576a46593c7e420c58039b0381b37f47535b5
Prabhat Sharma <hi.prabhat@gmail.com>
2016-01-16 06:47:34 +0530

    First page added

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'ps-app',
+    templateUrl: 'app/app.component.html'
+})
+
+export class AppComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
